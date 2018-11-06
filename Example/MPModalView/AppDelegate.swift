f887d25ed2598c1994575b06057f7bb05a5eef35
martin <martin@pfundmair.de>
2018-11-07 07:30:06 +0900

    fix minimum deployment targets

--- Example/MPModalView/AppDelegate.swift
+++ Example/MPModalView/AppDelegate.swift
@@ -13,8 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        
+    }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
--- Example/MPModalView/ViewController.swift
+++ Example/MPModalView/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MPModalView
 
 class ViewController: UIViewController {
 
