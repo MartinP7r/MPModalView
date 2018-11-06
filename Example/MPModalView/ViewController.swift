8c9e578f71e3e205c55f7ebed0e6443eaff8848c
martin <martin@pfundmair.de>
2018-11-07 07:49:07 +0900

    add example modals

--- Example/MPModalView/ViewController.swift
+++ Example/MPModalView/ViewController.swift
@@ -13,16 +13,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let popOver = PopoverVC(actionButtons: [ActionButton(type: .basic(title: "Bla")), ActionButton(type: .cancel),ActionButton(type: .basic(title: "Blu"))], contentView: .text(title: "A", body: "B"))
-
-        popOver.presentOnTop()
+        setupButtons()
+        //        let modal = PopoverVC(actionButtons: [ActionButton(type: .basic(title: "Bla")),
+        //                                              ActionButton(type: .cancel),
+        //                                              ActionButton(type: .basic(title: "Blu"))],
+        //                              contentView: .text(title: "A", body: "B"))
+        //        modal.presentOnTop()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupButtons() {
+        let btnOkModal = UIButton(type: .roundedRect)
+        btnOkModal.setTitle("with OK button", for: .normal)
+        let btnOkCancelModal = UIButton(type: .roundedRect)
+        btnOkCancelModal.setTitle("with OK&Cancel buttons", for: .normal)
+        let btnNoBtnModal = UIButton(type: .roundedRect)
+        btnNoBtnModal.setTitle("without buttons", for: .normal)
+
+        btnOkModal.addTarget(self, action: #selector(showOKBtnModal(sender:)), for: .touchUpInside)
+        btnOkCancelModal.addTarget(self, action: #selector(showOKCancelBtnModal(sender:)), for: .touchUpInside)
+        btnNoBtnModal.addTarget(self, action: #selector(showNOBtnModal(sender:)), for: .touchUpInside)
+
+        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 120, height: 350))
+        stackView.addArrangedSubview(btnOkModal)
+        stackView.addArrangedSubview(btnOkCancelModal)
+        stackView.addArrangedSubview(btnNoBtnModal)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
+    @objc func showOKBtnModal(sender: UIButton) {
+        let modal = PopoverVC(actionButtons: [ActionButton(type: .ok)],
+                              contentView: .text(title: "Modal", body: "with one OK button"))
+        modal.presentOnTop()
+    }
+
+    @objc func showOKCancelBtnModal(sender: UIButton) {
+        let modal = PopoverVC(actionButtons: [ActionButton(type: .cancel, { self.dismiss(animated: true) }),
+                                              ActionButton(type: .ok)],
+                              contentView: .text(title: "Modal", body: "with OK & Cancel button"))
+        modal.presentOnTop()
+    }
+
+    @objc func showNOBtnModal(sender: UIButton) {
+        let modal = PopoverVC(actionButtons: [],
+                              contentView: .text(title: "Modal", body: "without buttons"))
+        modal.presentOnTop()
+    }
 }
 
