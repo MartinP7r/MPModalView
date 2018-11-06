fa567531201c7d6af8268388f6b2fb7651affdd2
martin <martin@disco.co.jp>
2018-11-06 09:56:33 +0900

    Initial Commit

---
+++ MPModalView/MPModalView.h
@@ -0,0 +1,19 @@
+//
+//  MPModalView.h
+//  MPModalView
+//
+//  Created by Pfundmair Martin on 2018-11-06.
+//  Copyright © 2018 Martin Pfundmair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MPModalView.
+FOUNDATION_EXPORT double MPModalViewVersionNumber;
+
+//! Project version string for MPModalView.
+FOUNDATION_EXPORT const unsigned char MPModalViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPModalView/PublicHeader.h>
+
+
